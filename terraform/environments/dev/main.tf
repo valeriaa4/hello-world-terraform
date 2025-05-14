e3aa693c08a9413bc61a2db5f5c177bc124bd2b2
Valéria Santos <santosvaleriav4@gmail.com>
2025-05-14 15:10:52 -0300

    chore: removendo comments

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -1,25 +1,25 @@
-# module "lambda-um" {
-#   source        = "../../modules/lambda-um"
-#   function_name = var.hello_function_name
-#   handler       = var.hello_handler
-#   filename      = var.hello_filename
-# }
-#
-# module "lambda-dois" {
-#   source         = "../../modules/lambda-dois"
-#   function_name  = var.create_function_name
-#   handler        = var.create_handler
-#   filename       = var.create_filename
-#   table_name     = var.create_table_name
-# }
-#
-# module "lambda-tres" {
-#   source         = "../../modules/lambda-tres"
-#   function_name  = var.update_function_name
-#   handler        = var.update_handler
-#   filename       = var.update_filename
-#   table_name     = var.update_table_name
-# }
+module "lambda-um" {
+  source        = "../../modules/lambda-um"
+  function_name = var.hello_function_name
+  handler       = var.hello_handler
+  filename      = var.hello_filename
+}
+
+module "lambda-dois" {
+  source         = "../../modules/lambda-dois"
+  function_name  = var.create_function_name
+  handler        = var.create_handler
+  filename       = var.create_filename
+  table_name     = var.create_table_name
+}
+
+module "lambda-tres" {
+  source         = "../../modules/lambda-tres"
+  function_name  = var.update_function_name
+  handler        = var.update_handler
+  filename       = var.update_filename
+  table_name     = var.update_table_name
+}
 
 module "lambda-quatro" {
   source         = "../../modules/lambda-quatro"
