00faa6e732d2c64cf51313ad5e7dbd3d3c7d96ee
Valéria Santos <santosvaleriav4@gmail.com>
2025-05-19 09:05:16 -0300

    refactor: ajustando modulo dynamodb

--- infra/modules/dynamodb/main.tf
+++ infra/modules/dynamodb/main.tf
@@ -14,25 +14,3 @@ resource "aws_dynamodb_table" "market_list" {
     type = "S"
   }
 }
-
-resource "aws_iam_role_policy" "dynamodb_access" {
-  role = aws_iam_role.lambda_exec.name
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "dynamodb:GetItem",
-          "dynamodb:PutItem",
-          "dynamodb:UpdateItem",
-          "dynamodb:DeleteItem",
-          "dynamodb:Query",
-          "dynamodb:Scan"
-        ]
-        Resource = "arn:aws:dynamodb:*:*:table/MARKET_LIST"
-      }
-    ]
-  })
-}
