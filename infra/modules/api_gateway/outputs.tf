4ef55268d92f98f4efaf83385ee0fed1c5c993a6
Thais-Abe <abe.c.thais@gmail.com>
2025-05-28 14:54:04 -0300

    fix: code hello

--- infra/modules/api_gateway/outputs.tf
+++ infra/modules/api_gateway/outputs.tf
@@ -2,11 +2,6 @@ output "invoke_arn" {
   value = var.invoke_arn
 }
 
-output "function_name" {
-  description = "Nome da função Lambda hello"
-  value       = aws_lambda_function.hello_lambda.function_name
-}
-
 output "hello_function_name" {
   description = "Nome da função Lambda Hello recebida pelo módulo"
   value       = var.hello_function_name
