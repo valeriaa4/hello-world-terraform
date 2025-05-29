1f7d10154afa5aa6b28bb126df321e2b6dd60c4a
Thais-Abe <abe.c.thais@gmail.com>
2025-05-29 13:51:28 -0300

    test12: testando recurso lambda e dynamo

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -6,13 +6,13 @@
 #   value = module.cognito.user_pool_client_id
 # }
 
-output "function_name" {
-  value = module.hello_terraform.function_name
-}
+# output "function_name" {
+#   value = module.hello_terraform.function_name
+# }
 
-output "invoke_arn" {
-  value = module.hello_terraform.invoke_arn
-}
+# output "invoke_arn" {
+#   value = module.hello_terraform.invoke_arn
+# }
 
 
 
