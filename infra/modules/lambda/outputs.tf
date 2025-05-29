1e3639958629da99a2d4ac43aadb3b5425b5b124
Thais-Abe <abe.c.thais@gmail.com>
2025-05-29 17:28:56 -0300

    test30: testando recurso lambda e dynamo

--- infra/modules/lambda/outputs.tf
+++ infra/modules/lambda/outputs.tf
@@ -2,11 +2,11 @@ output "function_name" {
   value = aws_lambda_function.lambda.function_name
 }
 
-# output "invoke_arn" {
-#   value = aws_lambda_function.lambda.invoke_arn
-# }
-
-
-output "function_arn" {
-  value = aws_lambda_function.lambda.arn
+output "invoke_arn" {
+  value = aws_lambda_function.lambda.invoke_arn
 }
+
+
+# output "function_arn" {
+#   value = aws_lambda_function.lambda.arn
+# }
